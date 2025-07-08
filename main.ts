c2789c637a3e79dc1bd3a1903164be92d9d74b18
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-09 00:44:22 +0800

    配置eslint

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 import { launchBrowser } from './utils/browser'
 import logger from './utils/logger'
-;(async () => {
-  logger.info('程序启动')
+import { isLoggedIn, autoLogin } from './services/loginService'
+
+void (async () => {
+  logger.info('================ 程序启动 ================')
   const browser = await launchBrowser()
   const page = await browser.newPage()
+
   logger.info('浏览器启动')
   await page.goto('https://www.zhipin.com')
+
+  logger.info('开始判断是否登录')
+  const checkLogin = await isLoggedIn(page)
+  logger.info(`${checkLogin ? '已登录' : '未登录'}`)
+
+  if (!checkLogin) {
+    await autoLogin(page)
+  }
 })()
--- services/loginService.ts
+++ services/loginService.ts
@@ -1,7 +1,25 @@
 import { Page } from 'puppeteer-core'
+import logger from '../utils/logger'
 
-// 判断是否登录
+/**
+ * 判断当前页面是否已登录
+ *
+ * 该函数会查找页面上是否存在 BOSS直聘的“登录/注册”按钮（a[ka="header-login"]）。
+ * 如果找不到该按钮，说明已登录；如果能找到，说明未登录。
+ *
+ * @param {Page} page - Puppeteer 的 Page 实例，表示要检测的页面
+ * @returns {Promise<boolean>} - 已登录返回 true，未登录返回 false
+ */
 export async function isLoggedIn(page: Page): Promise<boolean> {
   const loginBtn = await page.$('a[ka="header-login"]')
   return !loginBtn
+}
+
+/**
+ * 自动登录函数
+ * @param page Puppeteer Page 实例
+ */
+export async function autoLogin(page: Page) {
+  const loginBtn = await page.$('a[ka="header-login"]')
+  console.log(loginBtn)
 }
