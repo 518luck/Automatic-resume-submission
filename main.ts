5c2217103ead844de8ded432d48ba1eea08e5cfd
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-08 23:34:12 +0800

    暂定,boss直聘吧后面在扩容其他招聘网站

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import { launchBrowser } from './utils/browser'
 ;(async () => {
+  //跳转网址
   const browser = await launchBrowser()
-
   const page = await browser.newPage()
-
   await page.goto('https://www.zhipin.com')
 })()
--- services/loginService.ts
+++ services/loginService.ts
@@ -1,1 +1,7 @@
- 
+import { Page } from 'puppeteer-core'
+
+// 判断是否登录
+export async function isLoggedIn(page: Page): Promise<boolean> {
+  const loginBtn = await page.$('a[ka="header-login"]')
+  return !loginBtn
+}
