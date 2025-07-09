6d1e54e58891712536cae1b1cfc20b7a58813a9b
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-09 21:59:17 +0800

    job职位搜索

--- pages/zhipin.ts
+++ pages/zhipin.ts
@@ -37,5 +37,17 @@ export async function selectCity(page: Page) {
     return
   }
 
+  const job = process.env.JOB
+  if (!job) {
+    logger.error('职位未配置')
+    return
+  }
   await page.waitForSelector('input[class="ipt-search"]', { timeout: 10000 })
+  await page.type('input[class="ipt-search"]', job, {
+    delay: Math.random() * 150 + 50,
+  })
+  logger.info('搜索输入完成')
+
+  await page.waitForSelector('ul[class="search-result"]', { timeout: 10000 })
+  logger.info('搜索结果出现')
 }
