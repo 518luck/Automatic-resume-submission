be56098420bc2fc6bab4946bf50a2a4955942167
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-10 23:59:36 +0800

    强迫症

--- main.ts
+++ main.ts
@@ -4,29 +4,33 @@ import { launchBrowser } from './utils/browser'
 import logger from './utils/logger'
 import { isLoggedIn, autoLogin } from './services/boos/loginService'
 import { selectCity, clickAllJobsAndCommunicate } from './pages/zhipin'
-import { fetchWoffFromPage } from './utils/woffFetcher'
+// import { fetchWoffFromPage } from './utils/woffFetcher'
 
 dotenv.config()
 
 void (async () => {
-  logger.info('================ 程序启动 ================')
-  const browser = await launchBrowser()
-  const page = await browser.newPage()
+  try {
+    logger.info('================ 程序启动 ================')
+    const browser = await launchBrowser()
+    const page = await browser.newPage()
 
-  logger.info('浏览器启动')
-  await page.goto('https://www.zhipin.com')
+    logger.info('浏览器启动')
+    await page.goto('https://www.zhipin.com')
 
-  /*   logger.info('开始获取 woff 字体')
+    /*   logger.info('开始获取 woff 字体')
   await fetchWoffFromPage(page, '../data/woff_fonts') */
 
-  logger.info('开始判断是否登录')
-  const checkLogin = await isLoggedIn(page)
-  logger.info(`${checkLogin ? '已登录' : '未登录'}`)
+    logger.info('开始判断是否登录')
+    const checkLogin = await isLoggedIn(page)
+    logger.info(`${checkLogin ? '已登录' : '未登录'}`)
 
-  if (!checkLogin) {
-    await autoLogin(page)
+    if (!checkLogin) {
+      await autoLogin(page)
+    }
+
+    await selectCity(page)
+    await clickAllJobsAndCommunicate(page)
+  } catch (err) {
+    logger.error('主流程发生异常: ' + (err as Error).message)
   }
-
-  await selectCity(page)
-  await clickAllJobsAndCommunicate(page)
 })()
